f75fb9f20394aac7d65b0152c9b737b8a5b72335
Michał Begejowicz <michal.begejowicz@codesthq.com>
2018-03-05 20:49:43 +0100

    Extract builder and verifier

--- lib/jwt/authorizer.rb
+++ lib/jwt/authorizer.rb
@@ -3,39 +3,15 @@
 require "jwt/authorizer/version"
 require "jwt"
 
+require "jwt/authorizer/builder"
 require "jwt/authorizer/configuration"
 require "jwt/authorizer/configurable"
+require "jwt/authorizer/verifier"
 
 module JWT
   class Authorizer
     include Configurable
-
-    def build(claims = {})
-      payload = default_claims.merge!(claims)
-      JWT.encode payload, secret[:private], algorithm
-    end
-
-    def verify(token)
-      JWT.decode token, secret[:public], true, decode_options
-    end
-
-    private
-
-    def default_claims
-      {}.tap do |result|
-        result[:exp] = (Time.now + expiry).to_i if expiry
-        result[:iss] = issuer if issuer
-      end
-    end
-
-    def decode_options
-      {}.tap do |result|
-        if allowed_issuers.any?
-          result[:iss] = allowed_issuers
-          result[:verify_iss] = true
-        end
-        result[:algorithm] = algorithm
-      end
-    end
+    include Builder
+    include Verifier
   end
 end
