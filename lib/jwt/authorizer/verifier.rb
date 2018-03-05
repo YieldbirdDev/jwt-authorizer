041ef2103a9f417367f6a99894aa4595089da51c
Michał Begejowicz <michal.begejowicz@codesthq.com>
2018-03-05 20:49:43 +0100

    Extract builder and verifier

---
+++ lib/jwt/authorizer/verifier.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module JWT
+  class Authorizer
+    module Verifier
+      def verify(token)
+        JWT.decode token, secret[:public], true, decode_options
+      end
+
+      private
+
+      def decode_options
+        {}.tap do |result|
+          if allowed_issuers.any?
+            result[:iss] = allowed_issuers
+            result[:verify_iss] = true
+          end
+          result[:algorithm] = algorithm
+        end
+      end
+    end
+  end
+end
